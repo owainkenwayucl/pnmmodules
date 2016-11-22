8643f6921d6b4a330144671fa515e3463f7e9e86
Owain Kenway <o.kenway@ucl.ac.uk>
2016-11-22 15:10:41 +0000

    Add PBM writer.

--- R/imageio.R
+++ R/imageio.R
@@ -16,8 +16,32 @@ writepgm <- function(d, colours=256, filename="output.pgm") {
   cat(colours, "\n", file=filename, append=TRUE) 
 
   for (j in 1:y) {
-      for (i in 1:x) {
-          cat(floor(d[j,i]), "\n", file=filename, append=TRUE)
+    for (i in 1:x) {
+      cat(floor(d[j,i]), "\n", file=filename, append=TRUE)
+    }
+  }
+}
+
+# Write out a matrix d as pbm.
+# - threshold = threshold at which a number is 1 rather than 0
+# - filename = file to write to
+#
+# NOTE: we convert d to integers via floor.
+writepbm <- function(d, threshold=0.5, filename="output.pbm") {
+  y = nrow(d)
+  x = ncol(d)
+ 
+  cat("P1\n", file=filename, append=FALSE)
+  cat("# Written by pnmmodules (https://github.com/owainkenwayucl/pnmmodules).\n", file=filename, append=TRUE)
+  cat(x, y, "\n", file=filename, append=TRUE) 
+
+  for (j in 1:y) {
+    for (i in 1:x) {
+      if (d[j,i] >= threshold) {          
+        cat(1, "\n", file=filename, append=TRUE)
+      } else {
+        cat(0, "\n", file=filename, append=TRUE)
       }
+    }
   }
 }

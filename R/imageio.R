8697ddc05677ab76ff772999b2bdf7d3b03c497a
Owain Kenway <o.kenway@ucl.ac.uk>
2016-11-22 20:57:38 +0000

    Correct info on colours => white value.
    I misunderstood the point of the levels field in PGM format - it's the max
    value (white), not the number of colours.

--- R/imageio.R
+++ R/imageio.R
@@ -2,18 +2,18 @@
 # Owain Kenway
 
 # Write out a matrix d as pgm.
-# - colours = number of (grey) colours
+# - white - white value (max colour value)
 # - filename = file to write to
 #
 # NOTE: we convert d to integers via floor.
-writepgm <- function(d, colours=256, filename="output.pgm") {
+writepgm <- function(d, white=255, filename="output.pgm") {
   y = nrow(d)
   x = ncol(d)
  
   cat("P2\n", file=filename, append=FALSE)
   cat("# Written by pnmmodules (https://github.com/owainkenwayucl/pnmmodules).\n", file=filename, append=TRUE)
   cat(x, y, "\n", file=filename, append=TRUE) 
-  cat(colours, "\n", file=filename, append=TRUE) 
+  cat(white, "\n", file=filename, append=TRUE) 
 
   for (j in 1:y) {
     for (i in 1:x) {

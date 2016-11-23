90dd1d57cb524a0380c3bc88788a10b3823dfb73
Owain Kenway <o.kenway@ucl.ac.uk>
2016-11-23 14:19:52 +0000

    Actually write correct headers.

--- go/src/pnmmodules/imageio.go
+++ go/src/pnmmodules/imageio.go
@@ -31,6 +31,10 @@ func Writepgm(d [][]int, white int, filename string) {
 // Write PGM header.
     file.WriteString("P2\n")
     file.WriteString("# Written by pnmmodules (https://github.com/owainkenwayucl/pnmmodules).\n")
+    file.WriteString(strconv.Itoa(x))
+    file.WriteString(" ")
+    file.WriteString(strconv.Itoa(y))
+    file.WriteString("\n")
     file.WriteString(strconv.Itoa(white))
     file.WriteString("\n")
 
@@ -63,6 +67,9 @@ func Writepbm(d [][]int, threshold float64, filename string) {
 // Write PBM header.
     file.WriteString("P1\n")
     file.WriteString("# Written by pnmmodules (https://github.com/owainkenwayucl/pnmmodules).\n")
+    file.WriteString(strconv.Itoa(x))
+    file.WriteString(" ")
+    file.WriteString(strconv.Itoa(y))
     file.WriteString("\n")
 
 // Write out d.
